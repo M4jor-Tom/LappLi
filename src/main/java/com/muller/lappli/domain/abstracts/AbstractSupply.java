bfb042864a836acb70284eb27769f703919f3360
Tom <tom-vautray@hotmail.fr>
2021-12-06 10:03:49 +0100

    Javadoc for class AbstractSupply

--- src/main/java/com/muller/lappli/domain/abstracts/AbstractSupply.java
+++ src/main/java/com/muller/lappli/domain/abstracts/AbstractSupply.java
@@ -2,23 +2,55 @@ package com.muller.lappli.domain.abstracts;
 
 import javax.persistence.MappedSuperclass;
 
+/**
+ * An abstract mother class for each Supply class
+ *
+ * A supply object refers to the instanciation of a CylindricComponent
+ * inside a Strand or Cable
+ */
 @MappedSuperclass
 public abstract class AbstractSupply {
 
+    /**
+     * The unity length which Muller uses to measure cables statistics
+     *
+     * It may be increased by the supplied CylindricComponent's overlength factor
+     */
     public static final Long UNITY_METRIC_QUANTITY = Long.valueOf(1000);
 
+    /**
+     * @return the apparitions of the CylindricComponent inside the final Cable
+     */
     public abstract Long getApparitions();
 
+    /**
+     * @return the diameter in milimeters of the CylindricComponent
+     */
     public abstract Double getMilimeterDiameter();
 
+    /**
+     * @return the linear mass in grams per meter of the CylindricComponent
+     */
     public abstract Double getGramPerMeterLinearMass();
 
+    /**
+     * @return the preparation time in hours of the supply operation
+     */
     public abstract Double getHourPreparationTime();
 
+    /**
+     * @return the execution time in hours of the supply operation
+     */
     public abstract Double getHourExecutionTime();
 
+    /**
+     * @return the speed in meters per second of the supply operation
+     */
     public abstract Double getMeterPerSecondSpeed();
 
+    /**
+     * @return the necessary quantity of that CylindricComponent to make the final Cable
+     */
     public Long getMeterQuantity() {
         return AbstractSupply.UNITY_METRIC_QUANTITY * getApparitions();
     }
