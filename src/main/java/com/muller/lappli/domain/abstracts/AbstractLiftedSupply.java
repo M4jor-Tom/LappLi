a50f87e79d095759ad34a614666f9e5cce972b4e
Tom <tom-vautray@hotmail.fr>
2021-12-06 10:13:20 +0100

    Javadoc for class AbstractLiftedSupply

--- src/main/java/com/muller/lappli/domain/abstracts/AbstractLiftedSupply.java
+++ src/main/java/com/muller/lappli/domain/abstracts/AbstractLiftedSupply.java
@@ -5,12 +5,21 @@ import java.util.List;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Transient;
 
+/**
+ * An abstract mother class for each Supply class that has
+ * a Lifting operation as a supply operation
+ */
 @MappedSuperclass
 public abstract class AbstractLiftedSupply extends AbstractSupply {
 
     @Transient
     private List<Lifter> bestLifterList;
 
+    /**
+     * Gets a list of each Lifter names for getBestLifterList()
+     *
+     * @return a List of Lifter names
+     */
     public String getBestLiftersNames() {
         if (getBestLifterList() == null) {
             return "";
@@ -25,10 +34,22 @@ public abstract class AbstractLiftedSupply extends AbstractSupply {
         return names;
     }
 
+    /**
+     * Gets a list of each Lifter that have the same, best
+     * set of statistics for the current supply
+     *
+     * @return a List of Lifter
+     */
     public List<Lifter> getBestLifterList() {
         return bestLifterList;
     }
 
+    /**
+     * This method shall only be called for setting bestLifterList by
+     * a service that is capable of determining it
+     *
+     * @param bestLifterList
+     */
     public void setBestLifterList(List<Lifter> bestLifterList) {
         this.bestLifterList = bestLifterList;
     }
