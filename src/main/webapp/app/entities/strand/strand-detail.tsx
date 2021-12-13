eada688f79baaa9da944db417423b105e2aa9f35
Tom <tom-vautray@hotmail.fr>
2021-12-13 13:58:16 +0100

    Added CustomComponentSupplies in strand-detail

--- src/main/webapp/app/entities/strand/strand-detail.tsx
+++ src/main/webapp/app/entities/strand/strand-detail.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { Button, Row, Col } from 'reactstrap';
+import { Button, Row, Col, Table } from 'reactstrap';
 import { Translate } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -41,6 +41,51 @@ export const StrandDetail = (props: RouteComponentProps<{ id: string }>) => {
             </span>
           </dt>
           <dd>{strandEntity.housingOperationType}</dd>
+          <dt>
+            <span id="customComponentSupplies">
+              <Translate contentKey="lappLiApp.strand.customComponentSupplies">Custom component supplies</Translate>
+            </span>
+          </dt>
+          <dd>
+            <div className="table-responsive">
+              {strandEntity.customComponentSupplies && strandEntity.customComponentSupplies.length > 0 ? (
+                <Table responsive>
+                  <tbody>
+                    {strandEntity.customComponentSupplies.map((customComponentSupply, i) => (
+                      <>
+                        <td>{customComponentSupply.apparitions}</td>
+                        <td>{customComponentSupply.description}</td>
+                        <td>
+                          <Translate contentKey={`lappLiApp.MarkingType.${customComponentSupply.markingType}`} />
+                        </td>
+                        <td>
+                          {customComponentSupply.customComponent ? (
+                            <Link to={`custom-component/${customComponentSupply.customComponent.id}`}>
+                              {customComponentSupply.customComponent.designation}
+                            </Link>
+                          ) : (
+                            ''
+                          )}
+                        </td>
+                        <td>{customComponentSupply.meterQuantity}</td>
+                        <td>{customComponentSupply.customComponent.milimeterDiameter}</td>
+                        <td>{customComponentSupply.customComponent.gramPerMeterLinearMass}</td>
+                        <td>{customComponentSupply.bestLiftersNames}</td>
+                        <td>{customComponentSupply.customComponent.surfaceMaterial?.designation}</td>
+                        <td>{customComponentSupply.customComponent.surfaceColor}</td>
+                        <td>{customComponentSupply.meterPerHourSpeed}</td>
+                        <td>{customComponentSupply.formatedHourPreparationTime}</td>
+                        <td>{customComponentSupply.formatedHourExecutionTime}</td>
+                        <td>{customComponentSupply.markingTechnique}</td>
+                      </>
+                    ))}
+                  </tbody>
+                </Table>
+              ) : (
+                <div>No custom components</div>
+              )}
+            </div>
+          </dd>
         </dl>
         <Button tag={Link} to="/strand" replace color="info" data-cy="entityDetailsBackButton">
           <FontAwesomeIcon icon="arrow-left" />{' '}
