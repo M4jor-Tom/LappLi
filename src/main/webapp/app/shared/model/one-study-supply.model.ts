8d02171e4a41d664de88b36c794c12160d41a7d3
Tom <tom-vautray@hotmail.fr>
2021-12-20 11:31:34 +0100

    Changed one-study-supply-model for inheritance

--- src/main/webapp/app/shared/model/one-study-supply.model.ts
+++ src/main/webapp/app/shared/model/one-study-supply.model.ts
@@ -2,19 +2,15 @@ import { IMaterial } from 'app/shared/model/material.model';
 import { IStrand } from 'app/shared/model/strand.model';
 import { MarkingType } from 'app/shared/model/enumerations/marking-type.model';
 import { Color } from 'app/shared/model/enumerations/color.model';
+import { IAbstractMarkedLiftedSupply } from './abstract-marked-lifted-supply.model';
 
-export interface IOneStudySupply {
-  id?: number;
-  apparitions?: number | null;
+export interface IOneStudySupply extends IAbstractMarkedLiftedSupply {
   number?: number | null;
   designation?: string | null;
-  description?: string | null;
-  markingType?: MarkingType;
   gramPerMeterLinearMass?: number;
   milimeterDiameter?: number;
   surfaceColor?: Color;
   surfaceMaterial?: IMaterial;
-  strand?: IStrand;
 }
 
 export const defaultValue: Readonly<IOneStudySupply> = {};
