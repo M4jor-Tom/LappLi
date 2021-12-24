2af36307b1ded2c512940781712dec430d4b85f6
Tom <tom-vautray@hotmail.fr>
2021-12-24 11:14:11 +0100

    add a strand-sub-supply go to button in study-supplies

--- src/main/webapp/app/entities/study/study-strand-supply.tsx
+++ src/main/webapp/app/entities/study/study-strand-supply.tsx
@@ -101,6 +101,19 @@ export const StudyStrandSupply = (props: RouteComponentProps<{ study_id: string 
                             </Button>*/}
                             <Button
                               tag={Link}
+                              to={`/strand/${strandSupply.strand.id}/supply`}
+                              color="primary"
+                              size="sm"
+                              data-cy="entityEditButton"
+                            >
+                              <FontAwesomeIcon icon="pencil-alt" />{' '}
+                              <span className="d-none d-md-inline">
+                                <Translate contentKey="lappLiApp.strand.subSupplies">Sub Supplies</Translate>
+                              </span>
+                            </Button>
+                            &nbsp;
+                            <Button
+                              tag={Link}
                               to={`/strand-supply/${strandSupply.id}/edit`}
                               color="primary"
                               size="sm"
@@ -111,6 +124,7 @@ export const StudyStrandSupply = (props: RouteComponentProps<{ study_id: string 
                                 <Translate contentKey="entity.action.edit">Edit</Translate>
                               </span>
                             </Button>
+                            &nbsp;
                             <Button
                               tag={Link}
                               to={`/strand-supply/${strandSupply.id}/delete`}
