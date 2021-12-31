f9380b181615e18ebd38c42b54a2f8bf981a57c5
Tom <tom-vautray@hotmail.fr>
2021-12-31 10:28:33 +0100

    Add enum SupplyState

---
+++ src/main/java/com/muller/lappli/domain/enumeration/SupplyState.java
@@ -0,0 +1,15 @@
+package com.muller.lappli.domain.enumeration;
+
+//TODO: Use that in Supply classes,
+//create AssemblySupply interface,
+//...
+public enum SupplyState {
+    //The Supply is in that state when not spread into a Strand
+    UNDIVIED,
+
+    //The Supply is in that state when spread into a Strand, but not into a Position
+    DIVIDED_UNPLACED,
+
+    //The supply is in that state when it has a Position
+    PLACED,
+}
