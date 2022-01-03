e9a12c324380dd418ef463782777a67452e85a7a
Tom <tom-vautray@hotmail.fr>
2022-01-03 16:43:58 +0100

    Add abstract-supply.model/supplyKind

--- src/main/webapp/app/shared/model/abstract-supply.model.ts
+++ src/main/webapp/app/shared/model/abstract-supply.model.ts
@@ -1,10 +1,12 @@
 import { ICylindricComponent } from './cylindric-component.model';
+import { SupplyKind } from './enumerations/supply-kind.model';
 import { IMaterial } from './material.model';
 import { IPosition } from './position.model';
 import { IStrand } from './strand.model';
 
 export interface IAbstractSupply {
   id?: number;
+  supplyKind?: SupplyKind;
   apparitions?: number;
   dividedApparitions?: number;
   apparitionsPositionsUsage?: number;
