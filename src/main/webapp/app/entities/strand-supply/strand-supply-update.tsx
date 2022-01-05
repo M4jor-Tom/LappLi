73b8d6a143a87031e2f363a4ff778498cb4a04d2
Tom <tom-vautray@hotmail.fr>
2022-01-05 17:07:00 +0100

    Hide strand selection field in strand-supply-update

--- src/main/webapp/app/entities/strand-supply/strand-supply-update.tsx
+++ src/main/webapp/app/entities/strand-supply/strand-supply-update.tsx
@@ -137,27 +137,35 @@ export const StrandSupplyUpdate = (props: RouteComponentProps<{ strand_id: strin
                 data-cy="description"
                 type="text"
               />
-              <ValidatedField
-                id="strand-supply-strand"
-                name="strand"
-                data-cy="strand"
-                label={translate('lappLiApp.strandSupply.strand')}
-                type="select"
-                value={strands ? strands.length : ''}
-                required
-              >
-                <option value="" key="0" />
-                {strands
-                  ? strands.map(otherEntity => (
-                      <option value={otherEntity.id} key={otherEntity.id}>
-                        {otherEntity.designation}
-                      </option>
-                    ))
-                  : null}
-              </ValidatedField>
-              <FormText>
-                <Translate contentKey="entity.validation.required">This field is required.</Translate>
-              </FormText>
+              {props.match.params.study_id ? (
+                ''
+              ) : (
+                <ValidatedField
+                  id="strand-supply-strand"
+                  name="strand"
+                  data-cy="strand"
+                  label={translate('lappLiApp.strandSupply.strand')}
+                  type="select"
+                  value={strands ? strands.length : ''}
+                  required
+                >
+                  <option value="" key="0" />
+                  {strands
+                    ? strands.map(otherEntity => (
+                        <option value={otherEntity.id} key={otherEntity.id}>
+                          {otherEntity.designation}
+                        </option>
+                      ))
+                    : null}
+                </ValidatedField>
+              )}
+              {props.match.params.study_id ? (
+                ''
+              ) : (
+                <FormText>
+                  <Translate contentKey="entity.validation.required">This field is required.</Translate>
+                </FormText>
+              )}
               {studyValidateField}
               <Button tag={Link} id="cancel-save" data-cy="entityCreateCancelButton" to={getOutUrl} replace color="info">
                 <FontAwesomeIcon icon="arrow-left" />
