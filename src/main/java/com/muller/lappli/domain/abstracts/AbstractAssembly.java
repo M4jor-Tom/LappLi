d4e493beef07e707330c8ad618324691c7ff61a0
Tom <tom-vautray@hotmail.fr>
2022-01-10 12:03:03 +0100

    Javadoc AbstractAssembly

--- src/main/java/com/muller/lappli/domain/abstracts/AbstractAssembly.java
+++ src/main/java/com/muller/lappli/domain/abstracts/AbstractAssembly.java
@@ -7,17 +7,38 @@ import com.muller.lappli.domain.exception.PositionInSeveralAssemblyException;
 import java.util.Set;
 import javax.persistence.MappedSuperclass;
 
+/**
+ * This class represents AbstractOperations which are Assemblies
+ */
 @MappedSuperclass
 public abstract class AbstractAssembly<T extends AbstractAssembly<T>> extends AbstractOperation<T> {
 
+    /**
+     * @return the strand which owns this assembly
+     */
     public abstract Strand getOwnerStrand();
 
+    /**
+     * @return a Set containing all positions in this
+     */
     public abstract Set<Position> getPositions();
 
+    /**
+     * @return the layer at which this operation is
+     */
     public abstract Long getOperationLayer();
 
+    /**
+     * @return the step at which this is producted
+     */
     public abstract Long getProductionStep();
 
+    /**
+     * Checks positions are all right with {@link Position#checkRight()}
+     *
+     * @throws PositionInSeveralAssemblyException if {@link Position#checkRight()} does
+     * @throws PositionHasSeveralSupplyException if {@link Position#checkRight()} does
+     */
     public void checkPositions() throws PositionInSeveralAssemblyException, PositionHasSeveralSupplyException {
         if (getPositions() != null) {
             for (Position position : getPositions()) {
@@ -26,6 +47,10 @@ public abstract class AbstractAssembly<T extends AbstractAssembly<T>> extends Ab
         }
     }
 
+    /**
+     * @return false if {@link #checkPositions()} throws PositionInSeveralAssemblyException or
+     * PositionHasSeveralSupplyException, true otherwise
+     */
     public Boolean positionsAreRight() {
         try {
             checkPositions();
