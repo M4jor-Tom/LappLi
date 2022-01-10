61e04024e026d819661d2282696d7415f01f0411
Tom <tom-vautray@hotmail.fr>
2022-01-10 12:04:44 +0100

    Javadoc AbstractCableAtom

--- src/main/java/com/muller/lappli/domain/abstracts/AbstractCableAtom.java
+++ src/main/java/com/muller/lappli/domain/abstracts/AbstractCableAtom.java
@@ -2,5 +2,8 @@ package com.muller.lappli.domain.abstracts;
 
 import javax.persistence.MappedSuperclass;
 
+/**
+ * This class represents AbstractCableries which are Atoms, unlike Composites
+ */
 @MappedSuperclass
 public abstract class AbstractCableAtom<T> extends AbstractCablery<T> {}
