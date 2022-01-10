61832f50476043ac158649840ead9078cf06ae73
Tom <tom-vautray@hotmail.fr>
2022-01-10 12:03:16 +0100

    Javadoc AbstractAssemblableAtom

--- src/main/java/com/muller/lappli/domain/abstracts/AbstractAssemblableAtom.java
+++ src/main/java/com/muller/lappli/domain/abstracts/AbstractAssemblableAtom.java
@@ -3,5 +3,8 @@ package com.muller.lappli.domain.abstracts;
 import com.muller.lappli.domain.interfaces.Assemblable;
 import javax.persistence.MappedSuperclass;
 
+/**
+ * This class represents AbstractCableries which are assemblables
+ */
 @MappedSuperclass
 public abstract class AbstractAssemblableAtom<T> extends AbstractCableAtom<T> implements Assemblable {}
