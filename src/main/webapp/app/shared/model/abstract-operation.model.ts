a981e5afbc06187e481e7c658bba94398f40796b
Tom <tom-vautray@hotmail.fr>
2022-01-12 12:13:17 +0100

    Add operation-kind in frontend

--- src/main/webapp/app/shared/model/abstract-operation.model.ts
+++ src/main/webapp/app/shared/model/abstract-operation.model.ts
@@ -1,6 +1,8 @@
+import { OperationKind } from './enumerations/operation-kind.model';
 import { IStrand } from './strand.model';
 
 export interface IAbstractOperation {
+  operationKind?: OperationKind;
   afterThisMilimeterDiameter?: number;
   operationLayer?: number;
   productionStep?: number;
