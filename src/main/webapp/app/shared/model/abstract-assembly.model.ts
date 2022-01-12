9ac9a60d07ade508af84b795048711781ccf73c4
Tom <tom-vautray@hotmail.fr>
2022-01-12 09:41:11 +0100

    Apply inheritance between AbstractAssembly & AbstractOperation in front

--- src/main/webapp/app/shared/model/abstract-assembly.model.ts
+++ src/main/webapp/app/shared/model/abstract-assembly.model.ts
@@ -1,12 +1,5 @@
 import { IAbstractOperation } from './abstract-operation.model';
-import { IPosition } from './position.model';
-import { IStrand } from './strand.model';
 
-export interface IAbstractAssembly extends IAbstractOperation {
-  operationLayer?: number;
-  productionStep?: number;
-  designation?: string | null;
-  ownerStrand?: IStrand;
-}
+export interface IAbstractAssembly extends IAbstractOperation {}
 
 export const defaultValue: Readonly<IAbstractAssembly> = {};
--- src/main/webapp/app/shared/model/abstract-operation.model.ts
+++ src/main/webapp/app/shared/model/abstract-operation.model.ts
@@ -1,5 +1,11 @@
+import { IStrand } from './strand.model';
+
 export interface IAbstractOperation {
   afterThisMilimeterDiameter?: number;
+  operationLayer?: number;
+  productionStep?: number;
+  designation?: string | null;
+  ownerStrand?: IStrand;
 }
 
 export const defaultValue: Readonly<IAbstractOperation> = {};
