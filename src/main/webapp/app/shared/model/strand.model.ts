de19ae109a5e33b5a2d9d17d526ed25a827208e4
Tom <tom-vautray@hotmail.fr>
2022-01-12 09:27:09 +0100

    Add Strand.operations in frontend

--- src/main/webapp/app/shared/model/strand.model.ts
+++ src/main/webapp/app/shared/model/strand.model.ts
@@ -6,10 +6,12 @@ import { ICustomComponentSupply } from 'app/shared/model/custom-component-supply
 import { IOneStudySupply } from 'app/shared/model/one-study-supply.model';
 import { ICentralAssembly } from 'app/shared/model/central-assembly.model';
 import { IStudy } from 'app/shared/model/study.model';
+import { IAbstractOperation } from './abstract-operation.model';
 
 export interface IStrand {
   id?: number;
   designation?: string;
+  operations?: IAbstractOperation[] | null;
   coreAssemblies?: ICoreAssembly[] | null;
   intersticeAssemblies?: IIntersticeAssembly[] | null;
   elementSupplies?: IElementSupply[] | null;
