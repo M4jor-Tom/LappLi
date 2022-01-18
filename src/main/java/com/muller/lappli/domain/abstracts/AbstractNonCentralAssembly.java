2b4eedc6deba4b12207bbc92dd96c683035ea9c4
Tom <tom-vautray@hotmail.fr>
2022-01-18 16:12:48 +0100

    Remove unused code in AbstractNonCentralAssembly

--- src/main/java/com/muller/lappli/domain/abstracts/AbstractNonCentralAssembly.java
+++ src/main/java/com/muller/lappli/domain/abstracts/AbstractNonCentralAssembly.java
@@ -1,9 +1,6 @@
 package com.muller.lappli.domain.abstracts;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.muller.lappli.domain.enumeration.AssemblyMean;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * This class represents an assembly, which is a layer of the {@link com.muller.lappli.domain.Strand} in {@link AbstractAssembly#getOwnerStrand()}
@@ -19,18 +16,6 @@ public abstract class AbstractNonCentralAssembly<T extends AbstractNonCentralAss
      * @return the assembly's mean
      */
     public abstract AssemblyMean getAssemblyMean();
-
-    @JsonIgnoreProperties(value = { "ownerStrand" })
-    public Set<AbstractSupply<?>> getSupplies() {
-        Set<AbstractSupply<?>> supplies = new HashSet<>();
-
-        //supplies.addAll(getBangleSupplies());
-        //supplies.addAll(getCustomComponentSupplies());
-        //supplies.addAll(getElementSupplies());
-        //supplies.addAll(getOneStudySupplies());
-
-        return supplies;
-    }
 
     @Override
     public String getProductDesignation() {
