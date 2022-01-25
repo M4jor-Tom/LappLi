c48108ba7b827b10acf12a59f0da3f3e35b07eb3
Tom <tom-vautray@hotmail.fr>
2022-01-25 15:45:50 +0100

    Update fronted models for assembly & abstract supply entities

--- src/main/webapp/app/shared/model/central-assembly.model.ts
+++ src/main/webapp/app/shared/model/central-assembly.model.ts
@@ -1,8 +1,10 @@
 import { IStrand } from 'app/shared/model/strand.model';
 import { IAbstractAssembly } from './abstract-assembly.model';
+import { ISupplyPosition } from './supply-position.model';
 
 export interface ICentralAssembly extends IAbstractAssembly {
   id?: number;
+  supplyPosition?: ISupplyPosition;
   //  productionStep?: number;
 }
 
