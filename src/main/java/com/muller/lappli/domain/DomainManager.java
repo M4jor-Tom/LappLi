e7d16deee10c0415bc0197f43a4e16e064478958
Tom <tom-vautray@hotmail.fr>
2022-02-08 10:03:26 +0100

    Add debug items in DomainManager

--- src/main/java/com/muller/lappli/domain/DomainManager.java
+++ src/main/java/com/muller/lappli/domain/DomainManager.java
@@ -15,4 +15,27 @@ public class DomainManager {
      * this returned will signal that an error has occured
      */
     public static final Long ERROR_LONG_POSITIVE_VALUE = Long.valueOf(-1);
+
+    /**
+     * A String to be used for prompting to help noticing
+     */
+    public static final String noticeBar = "\n\n=============\n\n";
+
+    /**
+     * @param toNotice the string to be returned surrounded
+     * by {@link DomainManager#noticeBar}
+     * @return the surrounded String
+     */
+    public static String noticeString(String toNotice) {
+        return noticeBar + toNotice + noticeBar;
+    }
+
+    /**
+     * Like {@link System.out.println()}, but prints with
+     *
+     * {@link DomainManager#noticeString()}
+     */
+    public static void noticeInPrompt(String toNotice) {
+        System.out.println(noticeString(toNotice));
+    }
 }
