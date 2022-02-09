bfbdaf6a1a70fbac69a595c4db6c4401df59094c
Tom <tom-vautray@hotmail.fr>
2022-02-09 16:21:04 +0100

    Revert "Remove unused imports"
    This reverts commit 115fa8ba8db3ae44554335336d9c685070c397ef.

--- src/test/java/com/muller/lappli/web/rest/UserResourceIT.java
+++ src/test/java/com/muller/lappli/web/rest/UserResourceIT.java
@@ -2,6 +2,7 @@ package com.muller.lappli.web.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -11,6 +12,7 @@ import com.muller.lappli.domain.User;
 import com.muller.lappli.repository.UserRepository;
 import com.muller.lappli.security.AuthoritiesConstants;
 import com.muller.lappli.service.dto.AdminUserDTO;
+import com.muller.lappli.service.dto.UserDTO;
 import com.muller.lappli.service.mapper.UserMapper;
 import com.muller.lappli.web.rest.vm.ManagedUserVM;
 import java.time.Instant;
