867045a361411b014519ed667e634875148e4189
Tom <tom-vautray@hotmail.fr>
2022-02-16 11:16:16 +0100

    Add class AssemblyPreset

---
+++ src/main/java/com/muller/lappli/domain/AssemblyPreset.java
@@ -0,0 +1,39 @@
+package com.muller.lappli.domain;
+
+public class AssemblyPreset {
+
+    private Long utilityComponentsCount;
+    private Long completionComponentsCount;
+
+    public AssemblyPreset() {}
+
+    public AssemblyPreset(int utilityComponentsCount, int completionComponentsCount) {
+        setUtilityComponentsCount(Long.valueOf(utilityComponentsCount));
+        setCompletionComponentsCount(Long.valueOf(completionComponentsCount));
+    }
+
+    public AssemblyPreset(Long utilityComponentsCount, Long completionComponentsCount) {
+        setUtilityComponentsCount(utilityComponentsCount);
+        setCompletionComponentsCount(completionComponentsCount);
+    }
+
+    public Long getTotalComponentsCount() {
+        return getUtilityComponentsCount() + getCompletionComponentsCount();
+    }
+
+    public Long getUtilityComponentsCount() {
+        return this.utilityComponentsCount;
+    }
+
+    public void setUtilityComponentsCount(Long utilityComponentsCount) {
+        this.utilityComponentsCount = utilityComponentsCount;
+    }
+
+    public Long getCompletionComponentsCount() {
+        return this.completionComponentsCount;
+    }
+
+    public void setCompletionComponentsCount(Long completionComponentsCount) {
+        this.completionComponentsCount = completionComponentsCount;
+    }
+}
