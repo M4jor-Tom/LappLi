173ebb6288471aff233886228bcb825c06e162d7
Tom <tom-vautray@hotmail.fr>
2022-02-17 14:45:23 +0100

    Add AssemblyPresetDistributionTest.correspondenceVerifier
    Checks count equality between AssemblyPresetDistribution and its calculator

--- src/test/java/com/muller/lappli/domain/enumeration/AssemblyPresetDistributionTest.java
+++ src/test/java/com/muller/lappli/domain/enumeration/AssemblyPresetDistributionTest.java
@@ -2,9 +2,16 @@ package com.muller.lappli.domain.enumeration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.muller.lappli.domain.interfaces.ICalculator;
 import org.junit.jupiter.api.Test;
 
 public class AssemblyPresetDistributionTest {
+
+    @Test
+    void correspondenceVerifier() {
+        assertThat(ICalculator.getInstance().assemblyPresetDistributionCalculatorCount())
+            .isEqualTo(Long.valueOf(AssemblyPresetDistribution.values().length));
+    }
 
     @Test
     void assemblyPresetDistributionPossibilitiesVerifier() throws Exception {
