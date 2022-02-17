3978977adf46d63e567c5272cd8441eb61b79538
Tom <tom-vautray@hotmail.fr>
2022-02-17 14:31:11 +0100

    AssemblyPresetDistributionTest.integrityVerifier rename
    to assemblyPresetDistributionPossibilitiesVerifier

--- src/test/java/com/muller/lappli/domain/enumeration/AssemblyPresetDistributionTest.java
+++ src/test/java/com/muller/lappli/domain/enumeration/AssemblyPresetDistributionTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class AssemblyPresetDistributionTest {
 
     @Test
-    void integrityVerifier() throws Exception {
+    void assemblyPresetDistributionPossibilitiesVerifier() throws Exception {
         for (AssemblyPresetDistribution assemblyPresetDistribution : AssemblyPresetDistribution.values()) {
             assertThat(assemblyPresetDistribution.assemblyPresetDistributionPossibilitiesAreConform()).isTrue();
         }
