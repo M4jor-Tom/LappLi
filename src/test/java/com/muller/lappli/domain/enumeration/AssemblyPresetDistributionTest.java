6414fb03290bbdd7b304e5e7c419bbd5f2e2d25e
Tom <tom-vautray@hotmail.fr>
2022-02-17 14:07:31 +0100

    Add AssemblyPresetDistributionTest

---
+++ src/test/java/com/muller/lappli/domain/enumeration/AssemblyPresetDistributionTest.java
@@ -0,0 +1,15 @@
+package com.muller.lappli.domain.enumeration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AssemblyPresetDistributionTest {
+
+    @Test
+    void integrityVerifier() throws Exception {
+        for (AssemblyPresetDistribution assemblyPresetDistribution : AssemblyPresetDistribution.values()) {
+            assertThat(assemblyPresetDistribution.assemblyPresetDistributionsAreConform()).isTrue();
+        }
+    }
+}
