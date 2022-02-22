cedce54120c167b5e94273e97fbb00a5653440ca
Tom <tom-vautray@hotmail.fr>
2022-02-22 16:49:33 +0100

    Remove unused import in CoreAssembly

--- src/main/java/com/muller/lappli/domain/CoreAssembly.java
+++ src/main/java/com/muller/lappli/domain/CoreAssembly.java
@@ -1,6 +1,5 @@
 package com.muller.lappli.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.muller.lappli.domain.abstracts.AbstractNonCentralAssembly;
 import com.muller.lappli.domain.abstracts.AbstractOperation;
