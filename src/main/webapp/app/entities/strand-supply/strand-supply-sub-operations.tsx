5a766a45af7daade1e62d59783ae54003357f037
Tom <tom-vautray@hotmail.fr>
2022-02-28 16:16:25 +0100

    Fix supply column for CentralAssemby in strand-supply-sub-operation

--- src/main/webapp/app/entities/strand-supply/strand-supply-sub-operations.tsx
+++ src/main/webapp/app/entities/strand-supply/strand-supply-sub-operations.tsx
@@ -99,8 +99,8 @@ export const StrandSupplySubOperation = (props: RouteComponentProps<{ strand_sup
                   <td>
                     <Translate contentKey="lappLiApp.centralAssembly.centralOperationLayer" />
                   </td>
-                  <td>{strandSupplyEntity.centralAssembly.productionStep}</td>
                   <td>{strandSupplyEntity.centralAssembly.supplyPosition.supply === null ? 0 : 1}</td>
+                  <td>{strandSupplyEntity.centralAssembly.productionStep}</td>
                   <td>{/* NO PRODUCT DESIGNATION (ASSEMBLY) */}</td>
                   <td>{strandSupplyEntity.centralAssembly.mullerStandardizedFormatMilimeterDiameterIncidency}</td>
                   <td>{strandSupplyEntity.centralAssembly.mullerStandardizedFormatAfterThisMilimeterDiameter}</td>
