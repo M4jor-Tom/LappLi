9ef70fba061aec933a95922290846c005513b0c0
Tom <tom-vautray@hotmail.fr>
2022-03-01 10:12:26 +0100

    Add AbstractAssembly.get(Completion|Utility)ComponentsCount to frontend

--- src/main/webapp/app/shared/model/abstract-assembly.model.ts
+++ src/main/webapp/app/shared/model/abstract-assembly.model.ts
@@ -4,6 +4,8 @@ import { IStrandSupply } from './strand-supply.model';
 export interface IAbstractAssembly extends IAbstractOperation {
   assemblyLayer?: number;
   ownerStrandSupply?: IStrandSupply;
+  completionComponentsCount?: number;
+  utilityComponentsCount?: number;
 }
 
 export const defaultValue: Readonly<IAbstractAssembly> = {};
