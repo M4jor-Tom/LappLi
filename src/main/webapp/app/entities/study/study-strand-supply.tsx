d9841e71b7e61c0b9ec5e5f5e8af27647bad2191
Tom <tom-vautray@hotmail.fr>
2022-03-02 17:17:28 +0100

    Remove strand edit button in study-strand-supply

--- src/main/webapp/app/entities/study/study-strand-supply.tsx
+++ src/main/webapp/app/entities/study/study-strand-supply.tsx
@@ -204,19 +204,6 @@ export const StudyStrandSupply = (props: RouteComponentProps<{ study_id: string 
                             &nbsp;
                             <Button
                               tag={Link}
-                              to={`${props.match.url}/strand/${strand.id}/edit`}
-                              color="primary"
-                              size="sm"
-                              data-cy="entityEditButton"
-                            >
-                              <FontAwesomeIcon icon="pencil-alt" />{' '}
-                              <span className="d-none d-md-inline">
-                                <Translate contentKey="entity.action.edit">Edit</Translate>
-                              </span>
-                            </Button>
-                            &nbsp;
-                            <Button
-                              tag={Link}
                               to={`${props.match.url}/strand/${strand.id}/delete`}
                               color="danger"
                               size="sm"
