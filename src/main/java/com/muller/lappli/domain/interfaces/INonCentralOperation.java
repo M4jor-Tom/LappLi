f62c6992f59f3ad21f4a60cda49aadbb545b7b6c
Tom <tom-vautray@hotmail.fr>
2022-03-08 14:19:17 +0100

    INonCentralOperation extends IOperation

--- src/main/java/com/muller/lappli/domain/interfaces/INonCentralOperation.java
+++ src/main/java/com/muller/lappli/domain/interfaces/INonCentralOperation.java
@@ -2,9 +2,7 @@ package com.muller.lappli.domain.interfaces;
 
 import com.muller.lappli.domain.abstracts.AbstractOperation;
 
-public interface INonCentralOperation<T extends INonCentralOperation<?>> {
-    public Long getOperationLayer();
-
+public interface INonCentralOperation<T extends INonCentralOperation<?>> extends IOperation<INonCentralOperation<T>> {
     public T operationLayer(Long operationLayer);
 
     public void setOperationLayer(Long operationLayer);
