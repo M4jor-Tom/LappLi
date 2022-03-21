0d333af2edfe49127dde7f2af91e6a78ff529cb9
Tom <tom-vautray@hotmail.fr>
2022-03-21 11:22:04 +0100

    Add StrandSupplyTest.nonCentralOperationInsertionTest

--- src/test/java/com/muller/lappli/domain/StrandSupplyTest.java
+++ src/test/java/com/muller/lappli/domain/StrandSupplyTest.java
@@ -20,4 +20,30 @@ class StrandSupplyTest {
         strandSupply1.setId(null);
         assertThat(strandSupply1).isNotEqualTo(strandSupply2);
     }
+
+    @Test
+    void nonCentralOperationInsertionTest() throws Exception {
+        StrandSupply strandSupply = new StrandSupply();
+
+        TapeLaying tapeLayingWhichMustRemainAtLayer1 = new TapeLaying().operationLayer(1L);
+        TapeLaying tapeLayingAtLayer2ThenShouldBeAtLayer3 = new TapeLaying().operationLayer(2L);
+        TapeLaying tapeLayingWhichShouldBeAtLayer3ThenAt4 = new TapeLaying().operationLayer(null);
+
+        strandSupply.insertNonCentralOperation(tapeLayingWhichMustRemainAtLayer1);
+        strandSupply.insertNonCentralOperation(tapeLayingAtLayer2ThenShouldBeAtLayer3);
+        strandSupply.insertNonCentralOperation(tapeLayingWhichShouldBeAtLayer3ThenAt4);
+
+        assertThat(tapeLayingWhichMustRemainAtLayer1.getOperationLayer()).isEqualTo(1L);
+        assertThat(tapeLayingAtLayer2ThenShouldBeAtLayer3.getOperationLayer()).isEqualTo(2L);
+        assertThat(tapeLayingWhichShouldBeAtLayer3ThenAt4.getOperationLayer()).isEqualTo(3L);
+
+        TapeLaying tapeLayingWhichTakesOperationLayer2 = new TapeLaying().operationLayer(2L);
+
+        strandSupply.insertNonCentralOperation(tapeLayingWhichTakesOperationLayer2);
+
+        assertThat(tapeLayingWhichMustRemainAtLayer1.getOperationLayer()).isEqualTo(1L);
+        assertThat(tapeLayingWhichTakesOperationLayer2.getOperationLayer()).isEqualTo(2L);
+        assertThat(tapeLayingAtLayer2ThenShouldBeAtLayer3.getOperationLayer()).isEqualTo(3L);
+        assertThat(tapeLayingWhichShouldBeAtLayer3ThenAt4.getOperationLayer()).isEqualTo(4L);
+    }
 }
