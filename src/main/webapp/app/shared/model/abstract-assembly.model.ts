8ab82132fdb297a921462706533f4cf0fe77bbc9
Tom <tom-vautray@hotmail.fr>
2022-03-22 14:28:27 +0100

    Add Type Guards
    AsbtractAssembly
    AbstractNonCentralAssembly
    CoreAssembly

--- src/main/webapp/app/shared/model/abstract-assembly.model.ts
+++ src/main/webapp/app/shared/model/abstract-assembly.model.ts
@@ -2,9 +2,15 @@ import { IAbstractOperation } from './abstract-operation.model';
 import { IStrandSupply } from './strand-supply.model';
 
 export interface IAbstractAssembly extends IAbstractOperation {
-  ownerStrandSupply?: IStrandSupply;
   completionComponentsCount?: number;
   utilityComponentsCount?: number;
+}
+
+export function isAssembly(object: IAbstractOperation): object is IAbstractAssembly {
+  return (
+    Object.prototype.hasOwnProperty.call(object, 'completionComponentsCount') &&
+    Object.prototype.hasOwnProperty.call(object, 'utilityComponentsCount')
+  );
 }
 
 export const defaultValue: Readonly<IAbstractAssembly> = {};
--- src/main/webapp/app/shared/model/core-assembly.model.ts
+++ src/main/webapp/app/shared/model/core-assembly.model.ts
@@ -1,10 +1,18 @@
 import { IStrand } from 'app/shared/model/strand.model';
 import { IAbstractNonCentralAssembly } from './abstract-non-central-assembly.model';
+import { IAbstractOperation } from './abstract-operation.model';
 
 export interface ICoreAssembly extends IAbstractNonCentralAssembly {
   id?: number;
   milimeterAssemblyVoid?: number;
   mullerStandardizedFormatMilimeterAssemblyVoid?: number;
+}
+
+export function isCoreAssembly(object: IAbstractOperation): object is ICoreAssembly {
+  return (
+    Object.prototype.hasOwnProperty.call(object, 'milimeterAssemblyVoid') &&
+    Object.prototype.hasOwnProperty.call(object, 'mullerStandardizedFormatMilimeterAssemblyVoid')
+  );
 }
 
 export const defaultValue: Readonly<ICoreAssembly> = {};
