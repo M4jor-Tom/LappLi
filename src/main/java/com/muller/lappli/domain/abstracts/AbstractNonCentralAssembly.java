dae7b45d9dcde38574c0397effd35034e9859cd4
Tom <tom-vautray@hotmail.fr>
2022-04-06 10:21:25 +0200

    AbstractNonCentralAssembly extends MeanedAssemblableOperation

--- src/main/java/com/muller/lappli/domain/abstracts/AbstractNonCentralAssembly.java
+++ src/main/java/com/muller/lappli/domain/abstracts/AbstractNonCentralAssembly.java
@@ -1,29 +1,19 @@
 package com.muller.lappli.domain.abstracts;
 
-import com.muller.lappli.domain.enumeration.AssemblyMean;
 import com.muller.lappli.domain.interfaces.INonCentralOperation;
 import com.muller.lappli.domain.interfaces.IOperation;
+import com.muller.lappli.domain.interfaces.MeanedAssemblableOperation;
 
 /**
  * This class represents an assembly, which is a layer of the {@link com.muller.lappli.domain.Strand} in {@link AbstractAssembly#getOwnerStrand()}
  */
 public abstract class AbstractNonCentralAssembly<T extends AbstractNonCentralAssembly<T>>
     extends AbstractAssembly<T>
-    implements INonCentralOperation<T> {
+    implements INonCentralOperation<T>, MeanedAssemblableOperation<T> {
 
     public AbstractNonCentralAssembly() {
         super();
     }
-
-    /**
-     * @return the assembly step measured in {@link IOperation#getAfterThisMilimeterDiameter}s
-     */
-    public abstract Double getDiameterAssemblyStep();
-
-    /**
-     * @return the assembly's mean
-     */
-    public abstract AssemblyMean getAssemblyMean();
 
     @Override
     public String getProductDesignation() {
