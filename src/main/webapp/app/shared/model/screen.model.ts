890b9c1fce8544c66c2dc8f6a51622b73e02160b
Tom <tom-vautray@hotmail.fr>
2022-04-07 10:54:54 +0200

    Screen & StripLaying models extends IMeanedAssemblableOperation

--- src/main/webapp/app/shared/model/screen.model.ts
+++ src/main/webapp/app/shared/model/screen.model.ts
@@ -2,12 +2,11 @@ import { ICopperFiber } from 'app/shared/model/copper-fiber.model';
 import { IStrandSupply } from 'app/shared/model/strand-supply.model';
 import { IAbstractOperation } from './abstract-operation.model';
 import { AssemblyMean } from './enumerations/assembly-mean.model';
+import { IMeanedAssemblableOperation } from './meaned-assemblable-operation.model';
 
-export interface IScreen extends IAbstractOperation {
+export interface IScreen extends IMeanedAssemblableOperation {
   id?: number;
   assemblyMeanIsSameThanAssemblys?: boolean;
-  assemblyMean?: AssemblyMean;
-  diameterAssemblyStep?: number;
   forcedDiameterAssemblyStep?: number | null;
   copperFiber?: ICopperFiber;
 }
