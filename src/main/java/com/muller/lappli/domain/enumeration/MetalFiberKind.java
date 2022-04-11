c8acc93f59c235412083299f3eade0025cd6d241
Tom <tom-vautray@hotmail.fr>
2022-04-11 15:34:30 +0200

    MetalFiberKinds now specify support for ContinuityWires, Screens, Plaits

--- src/main/java/com/muller/lappli/domain/enumeration/MetalFiberKind.java
+++ src/main/java/com/muller/lappli/domain/enumeration/MetalFiberKind.java
@@ -4,7 +4,43 @@ package com.muller.lappli.domain.enumeration;
  * The MetalFiberKind enumeration.
  */
 public enum MetalFiberKind {
-    RED_COPPER,
-    TINNED_COPPER,
-    STEEL,
+    RED_COPPER(true, true, true),
+    TINNED_COPPER(true, true, true),
+    STEEL(true, false, true);
+
+    private Boolean supportedForContinuityWire;
+
+    private Boolean supportedForScreen;
+
+    private Boolean supportedForPlait;
+
+    private MetalFiberKind(Boolean supportedForContinuityWire, Boolean supportedForScreen, Boolean supportedForPlait) {
+        setSupportedForContinuityWire(supportedForContinuityWire);
+        setSupportedForScreen(supportedForScreen);
+        setSupportedForPlait(supportedForPlait);
+    }
+
+    public Boolean getSupportedForContinuityWire() {
+        return this.supportedForContinuityWire;
+    }
+
+    private void setSupportedForContinuityWire(Boolean supportedForContinuityWire) {
+        this.supportedForContinuityWire = supportedForContinuityWire;
+    }
+
+    public Boolean getSupportedForScreen() {
+        return this.supportedForScreen;
+    }
+
+    private void setSupportedForScreen(Boolean supportedForScreen) {
+        this.supportedForScreen = supportedForScreen;
+    }
+
+    public Boolean getSupportedForPlait() {
+        return this.supportedForPlait;
+    }
+
+    private void setSupportedForPlait(Boolean supportedForPlait) {
+        this.supportedForPlait = supportedForPlait;
+    }
 }
