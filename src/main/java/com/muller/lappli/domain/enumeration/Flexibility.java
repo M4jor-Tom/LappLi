b2943e9df7ea1706117cddc7919e5e467ec12e85
Tom <tom-vautray@hotmail.fr>
2022-04-13 16:15:03 +0200

    Add Flexibility.{designation,proposed}

--- src/main/java/com/muller/lappli/domain/enumeration/Flexibility.java
+++ src/main/java/com/muller/lappli/domain/enumeration/Flexibility.java
@@ -4,15 +4,40 @@ package com.muller.lappli.domain.enumeration;
  * The Flexibility enumeration.
  */
 public enum Flexibility {
-    S,
-    ES,
-    SES,
-    MB,
-    UES,
-    USES,
-    DS,
-    R,
-    SS,
-    L5,
-    L6,
+    S("S", true),
+    ES("ES", true),
+    SES("SES", true),
+    MB("MB", true),
+    UES("UES", true),
+    USES("USES", false),
+    DS("DS", true),
+    R("", true),
+    SS("SS", false),
+    L5("L5", false),
+    L6("L6", false);
+
+    private String designation;
+
+    private Boolean proposed;
+
+    private Flexibility(String designation, Boolean proposed) {
+        setDesignation(designation);
+        setProposed(proposed);
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    private void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public Boolean isProposed() {
+        return proposed;
+    }
+
+    private void setProposed(Boolean proposed) {
+        this.proposed = proposed;
+    }
 }
