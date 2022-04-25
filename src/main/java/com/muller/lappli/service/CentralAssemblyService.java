cf00e4659e4051ff52d9828edab2fa7eedfa75b3
Tom <tom-vautray@hotmail.fr>
2022-04-25 15:12:32 +0200

    Remove repetitive method declarations in Assembly service classes

--- src/main/java/com/muller/lappli/service/CentralAssemblyService.java
+++ src/main/java/com/muller/lappli/service/CentralAssemblyService.java
@@ -1,48 +1,8 @@
 package com.muller.lappli.service;
 
 import com.muller.lappli.domain.CentralAssembly;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Service Interface for managing {@link CentralAssembly}.
  */
-public interface CentralAssemblyService extends IAssemblyService<CentralAssembly> {
-    /**
-     * Save a centralAssembly.
-     *
-     * @param centralAssembly the entity to save.
-     * @return the persisted entity.
-     */
-    CentralAssembly save(CentralAssembly centralAssembly);
-
-    /**
-     * Partially updates a centralAssembly.
-     *
-     * @param centralAssembly the entity to update partially.
-     * @return the persisted entity.
-     */
-    Optional<CentralAssembly> partialUpdate(CentralAssembly centralAssembly);
-
-    /**
-     * Get all the centralAssemblies.
-     *
-     * @return the list of entities.
-     */
-    List<CentralAssembly> findAll();
-
-    /**
-     * Get the "id" centralAssembly.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
-    Optional<CentralAssembly> findOne(Long id);
-
-    /**
-     * Delete the "id" centralAssembly.
-     *
-     * @param id the id of the entity.
-     */
-    void delete(Long id);
-}
+public interface CentralAssemblyService extends IAssemblyService<CentralAssembly> {}
