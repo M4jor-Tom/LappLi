8f7e0b5fe2f9ceea35687e48b3fcf26fd9b63d03
Tom <tom-vautray@hotmail.fr>
2022-04-26 09:42:22 +0200

    IDomainObject represents AbstractDomainObject methods

--- src/main/java/com/muller/lappli/domain/interfaces/IDomainObject.java
+++ src/main/java/com/muller/lappli/domain/interfaces/IDomainObject.java
@@ -1,8 +1,20 @@
 package com.muller.lappli.domain.interfaces;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Optional;
 
 public interface IDomainObject<T extends IDomainObject<T>> {
+    public Optional<T> getThisIfConform();
+
+    /**
+     * @return the Id of the object in the database
+     */
+    public Long getId();
+
+    public T id(Long id);
+
+    public void setId(Long id);
+
     /**
      * @return the object at its concretest state
      */
