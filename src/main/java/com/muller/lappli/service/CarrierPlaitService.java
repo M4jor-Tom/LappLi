b6e651cd4cc9164b2ad2e3e8dd94d356e131321c
Tom <tom-vautray@hotmail.fr>
2022-04-28 09:51:25 +0200

    CarrierPlaitService stops defining CRUD methods

--- src/main/java/com/muller/lappli/service/CarrierPlaitService.java
+++ src/main/java/com/muller/lappli/service/CarrierPlaitService.java
@@ -1,40 +1,8 @@
 package com.muller.lappli.service;
 
 import com.muller.lappli.domain.CarrierPlait;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Service Interface for managing {@link CarrierPlait}.
  */
-public interface CarrierPlaitService extends INonCentralOperationService<CarrierPlait> {
-    /**
-     * Partially updates a carrierPlait.
-     *
-     * @param carrierPlait the entity to update partially.
-     * @return the persisted entity.
-     */
-    Optional<CarrierPlait> partialUpdate(CarrierPlait carrierPlait);
-
-    /**
-     * Get all the carrierPlaits.
-     *
-     * @return the list of entities.
-     */
-    List<CarrierPlait> findAll();
-
-    /**
-     * Get the "id" carrierPlait.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
-    Optional<CarrierPlait> findOne(Long id);
-
-    /**
-     * Delete the "id" carrierPlait.
-     *
-     * @param id the id of the entity.
-     */
-    void delete(Long id);
-}
+public interface CarrierPlaitService extends INonCentralOperationService<CarrierPlait> {}
