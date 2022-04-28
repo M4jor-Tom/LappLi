ae615459ed93b07250f7c6a9a1ae1ab1cee97a67
Tom <tom-vautray@hotmail.fr>
2022-04-28 09:48:45 +0200

    NonCentralOperation services stop defining CRUD methods
    Because they are defined in INonCentralOperationService

--- src/main/java/com/muller/lappli/service/ScreenService.java
+++ src/main/java/com/muller/lappli/service/ScreenService.java
@@ -1,40 +1,8 @@
 package com.muller.lappli.service;
 
 import com.muller.lappli.domain.Screen;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Service Interface for managing {@link Screen}.
  */
-public interface ScreenService extends INonCentralOperationService<Screen> {
-    /**
-     * Partially updates a screen.
-     *
-     * @param screen the entity to update partially.
-     * @return the persisted entity.
-     */
-    Optional<Screen> partialUpdate(Screen screen);
-
-    /**
-     * Get all the screens.
-     *
-     * @return the list of entities.
-     */
-    List<Screen> findAll();
-
-    /**
-     * Get the "id" screen.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
-    Optional<Screen> findOne(Long id);
-
-    /**
-     * Delete the "id" screen.
-     *
-     * @param id the id of the entity.
-     */
-    void delete(Long id);
-}
+public interface ScreenService extends INonCentralOperationService<Screen> {}
