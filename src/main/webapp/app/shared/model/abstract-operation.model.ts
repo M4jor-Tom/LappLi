9ef706c18403c980c331dc11339cca9cc911b4d9
Tom <tom-vautray@hotmail.fr>
2022-05-16 17:12:00 +0200

    Add Muller Standardized Format field for Operation.hourExecutionTime
    Apply it in frontend

--- src/main/webapp/app/shared/model/abstract-operation.model.ts
+++ src/main/webapp/app/shared/model/abstract-operation.model.ts
@@ -8,6 +8,7 @@ export interface IAbstractOperation {
   productDesignation?: string;
   milimeterDiameterIncidency?: number;
   mullerStandardizedFormatMilimeterDiameterIncidency?: string;
+  mullerStandardizedFormatHourExecutionTime?: string;
   beforeThisMilimeterDiameter?: number;
   mullerStandardizedFormatBeforeThisMilimeterDiameter?: string;
   afterThisMilimeterDiameter?: number;
