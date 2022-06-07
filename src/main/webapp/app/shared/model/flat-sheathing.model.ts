eaee12d4e3622614a25678e87b9e3a000893c5ff
Tom <tom-vautray@hotmail.fr>
2022-06-07 13:52:34 +0200

    Add type guard for IFlatSheathing
    Also extends IAbstractOperation

--- src/main/webapp/app/shared/model/flat-sheathing.model.ts
+++ src/main/webapp/app/shared/model/flat-sheathing.model.ts
@@ -1,13 +1,16 @@
 import { IMaterial } from 'app/shared/model/material.model';
 import { IStrandSupply } from 'app/shared/model/strand-supply.model';
+import { IAbstractOperation } from './abstract-operation.model';
 
-export interface IFlatSheathing {
+export interface IFlatSheathing extends IAbstractOperation {
   id?: number;
-  operationLayer?: number;
   milimeterWidth?: number;
   milimeterHeight?: number;
   material?: IMaterial;
-  ownerStrandSupply?: IStrandSupply;
+}
+
+export function isFlatSheathing(object: IAbstractOperation): object is IFlatSheathing {
+  return Object.prototype.hasOwnProperty.call(object, 'milimeterWidth') && Object.prototype.hasOwnProperty.call(object, 'milimeterHeight');
 }
 
 export const defaultValue: Readonly<IFlatSheathing> = {};
