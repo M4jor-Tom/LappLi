d6a0c06bf00f05c4f775833d425b7844b9aa005c
Tom <tom-vautray@hotmail.fr>
2022-06-07 15:33:36 +0200

    Add TypeGuard for ISheathing Use ISheathing TypeGuard Fix ISheathing frontend definition

--- src/main/webapp/app/shared/model/sheathing.model.ts
+++ src/main/webapp/app/shared/model/sheathing.model.ts
@@ -3,10 +3,17 @@ import { SheathingKind } from 'app/shared/model/enumerations/sheathing-kind.mode
 import { IAbstractOperation } from './abstract-operation.model';
 
 export interface ISheathing extends IAbstractOperation {
-  thickness?: number;
   milimeterThickness?: number;
   sheathingKind?: SheathingKind;
   material?: IMaterial;
+}
+
+export function isSheathing(object: IAbstractOperation): object is ISheathing {
+  return (
+    Object.prototype.hasOwnProperty.call(object, 'milimeterThickness') &&
+    Object.prototype.hasOwnProperty.call(object, 'sheathingKind') &&
+    Object.prototype.hasOwnProperty.call(object, 'material')
+  );
 }
 
 export const defaultValue: Readonly<ISheathing> = {};
