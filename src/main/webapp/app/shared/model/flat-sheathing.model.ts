2c87740eb46e619551bf58b70288034d15adfac5
Tom <tom-vautray@hotmail.fr>
2022-06-07 17:14:01 +0200

    IFlatSheathing & ISheathing extend IAbstractSheathing

--- src/main/webapp/app/shared/model/flat-sheathing.model.ts
+++ src/main/webapp/app/shared/model/flat-sheathing.model.ts
@@ -1,22 +1,20 @@
 import { IMaterial } from 'app/shared/model/material.model';
 import { IStrandSupply } from 'app/shared/model/strand-supply.model';
 import { IAbstractOperation } from './abstract-operation.model';
+import { IAbstractSheathing, isAbstractSheathing } from './abstract-sheathing.model';
 import { SheathingKind } from './enumerations/sheathing-kind.model';
 
-export interface IFlatSheathing extends IAbstractOperation {
+export interface IFlatSheathing extends IAbstractSheathing {
   id?: number;
-  sheathingKind?: SheathingKind;
   milimeterWidth?: number;
   milimeterHeight?: number;
-  material?: IMaterial;
 }
 
 export function isFlatSheathing(object: IAbstractOperation): object is IFlatSheathing {
   return (
-    Object.prototype.hasOwnProperty.call(object, 'sheathingKind') &&
+    isAbstractSheathing(object) &&
     Object.prototype.hasOwnProperty.call(object, 'milimeterHeight') &&
-    Object.prototype.hasOwnProperty.call(object, 'milimeterHeight') &&
-    Object.prototype.hasOwnProperty.call(object, 'material')
+    Object.prototype.hasOwnProperty.call(object, 'milimeterHeight')
   );
 }
 
