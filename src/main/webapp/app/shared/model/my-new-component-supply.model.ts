acf49f3b40a0a44afa4a5acf55cf3de0f6d2f92a
Tom <tom-vautray@hotmail.fr>
2022-07-04 15:53:32 +0200

    MyNewComponentSupply as Supply in frontend strand.model amend

--- src/main/webapp/app/shared/model/my-new-component-supply.model.ts
+++ src/main/webapp/app/shared/model/my-new-component-supply.model.ts
@@ -1,11 +1,10 @@
 import { ISupplyPosition } from 'app/shared/model/supply-position.model';
 import { IMyNewComponent } from 'app/shared/model/my-new-component.model';
 import { MarkingType } from 'app/shared/model/enumerations/marking-type.model';
+import { IAbstractSupply } from './abstract-supply.model';
 
-export interface IMyNewComponentSupply {
+export interface IMyNewComponentSupply extends IAbstractSupply {
   id?: number;
-  apparitions?: number;
-  description?: string | null;
   markingType?: MarkingType;
   ownerSupplyPositions?: ISupplyPosition[];
   myNewComponent?: IMyNewComponent;
